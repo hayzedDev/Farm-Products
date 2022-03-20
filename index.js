f5d4be432b0c327b21fe7866a61092663e23e916
azeezade <azeezade04@gmail.com>
2022-03-20 23:58:01 +0100

    Another commit

--- index.js
+++ index.js
@@ -98,5 +98,5 @@ const server = http.createServer((req, res) => {
 const port = process.env.PORT || 8000;
 
 server.listen(port, "127.0.0.1", () => {
-  console.log(`Listening to request on port 8000`);
+  console.log(`Listening to request on port ${port}`);
 });
